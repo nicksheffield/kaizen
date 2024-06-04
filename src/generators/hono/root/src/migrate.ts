fd29777a80c9e53e98bd6188e7d7af0c061009d8
Nick Sheffield <numbereft@gmail.com>
2024-06-04 13:41:09 +1200

    update ns-migrate version

--- src/generators/hono/root/src/migrate.ts
+++ src/generators/hono/root/src/migrate.ts
@@ -1,6 +1,6 @@
 const tmpl = () => {
 	return `import { env, isDev } from './lib/env.js'
-	import { migrate as nsMigrate } from 'ns-migrate'
+	import { migrate as nsMigrate, LogLevel } from 'ns-migrate'
 	import { readFileSync } from 'node:fs'
 	import { fileURLToPath } from 'node:url'
 	import path from 'node:path'
@@ -11,7 +11,7 @@ const tmpl = () => {
 		return nsMigrate(
 			env.DB_URI,
 			JSON.parse(readFileSync(path.join(dirname, '../schema.json'), 'utf8')),
-			{ log: true, force: isDev }
+			{ log: LogLevel.log, force: isDev }
 		)
 	}
 	

93e9808188e998da4b6c74b3c853ddf1f2741c46
Nick Sheffield <numbereft@gmail.com>
2024-07-22 15:29:29 +1200

    add more convenient dev scripts for project

--- src/generators/workspace/root/packageJson.ts
+++ src/generators/workspace/root/packageJson.ts
@@ -3,6 +3,7 @@ import { ProjectCtx } from '@/generators/workspace/types'
 const tmpl = ({ project }: { project?: ProjectCtx }) => {
 	const scripts: Record<string, string> = {
 		start: 'cd apps/server && tsx src/index.ts',
+		'dev-server': 'cd apps/server && pnpm run dev',
 	}
 
 	const dependencies: Record<string, string> = {
@@ -12,6 +13,7 @@ const tmpl = ({ project }: { project?: ProjectCtx }) => {
 
 	if (project?.settings.hasClient) {
 		scripts.build = 'cd apps/client && vite build'
+		scripts['dev-client'] = 'cd apps/client && pnpm run dev'
 		dependencies.vite = '^5.1.4'
 	}
 

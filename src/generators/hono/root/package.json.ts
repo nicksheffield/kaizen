90540287838b56aa52200d9d47588d4ff9edbf8a
Nick Sheffield <numbereft@gmail.com>
2024-07-30 10:16:59 +1200

    add sentry to package.json

--- src/generators/hono/root/package.json.ts
+++ src/generators/hono/root/package.json.ts
@@ -24,6 +24,8 @@ const tmpl = () => {
 			'@lucia-auth/adapter-drizzle': '^1.0.3',
 			'@react-email/components': '^0.0.16',
 			'@react-email/render': '^0.0.12',
+			'@sentry/node': '^8.20.0',
+			'@sentry/profiling-node': '^8.20.0',
 			chalk: '^5.3.0',
 			'date-fns': '^3.6.0',
 			dotenv: '^16.4.5',
